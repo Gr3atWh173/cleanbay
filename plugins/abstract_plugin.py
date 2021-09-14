03232c10697f5e68bf78db5c31d22af3fa723820
Gr3atWh173 <Gr3atWh173@users.noreply.github.com>
2021-09-14 15:56:40 +0530

    add docstrings and signatures to abstract_plugin

--- plugins/abstract_plugin.py
+++ plugins/abstract_plugin.py
@@ -1,15 +1,27 @@
-from abc import ABC, abstractmethod
+"""The module contains the abstract interface for plugins"""
+from abc import ABC
 
-class CleanBay:
-	class AbstractPlugin(ABC):
-		def verify_cbplugin(self):
-			pass
+class AbstractPlugin(ABC):
+  """All plugins must be derived from this abstract class."""
+  def verify_cbplugin(self) -> bool:
+    """Verifies if this is indeed a valid CBPlugin. Must return `True`."""
+    pass
 
-		def verify_status(self):
-			pass
+  def verify_status(self) -> bool:
+    """Verifies the status of the external service used by the plugin.
+    Returns `True` only if said service is online and usable;
+    'False' otherwise."""
+    pass
 
-		def search(self):
-			pass
+  def search(self, search_param: str) -> list:
+    """Searches the external service and returns a list of `Torrent` objects.
 
-		def info(self):
-			pass
+    Keyword Arguments:
+    search_param -- the string to search for.
+    """
+    pass
+
+  def info(self) -> dict:
+    """Gives metadata about the plugin in the form of a dictionary.
+    Must include a unique `name` field."""
+    pass
