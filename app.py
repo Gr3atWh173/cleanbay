65fc8b4839227c485f8215573eba4fc1abdd3439
Gr3atWh173 <Gr3atWh173@users.noreply.github.com>
2021-09-14 15:58:05 +0530

    update the test

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ from backend import Backend
 back = Backend()
 
 print(back.config, back.plugins)
+
+back.search("Star trek")
+back.search("Star wars")
+back.search("Fast and the Furious")
+back.search("Star Trek")
+
+print(back.cache)
