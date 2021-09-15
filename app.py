34865a04f6159a43bbcd18b156b4e54151a55383
Gr3atWh173 <Gr3atWh173@users.noreply.github.com>
2021-09-15 14:02:46 +0530

    update test

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ back = Backend()
 print(back.config, back.plugins)
 
 back.search("dont breathe")
-back.search("god of war")
-back.search("f9")
-back.search("god of war")
+# back.search("god of war")
+# back.search("f9")
+# back.search("god of war")
 
 print(back.cache)
