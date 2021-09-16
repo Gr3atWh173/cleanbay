f962c4ac4d3442df36798d2949abedb3d02d414e
Gr3atWh173 <Gr3atWh173@users.noreply.github.com>
2021-09-16 18:30:34 +0530

    small refactor

--- backend/__init__.py
+++ backend/__init__.py
@@ -111,7 +111,7 @@ class Backend:
   def load_plugins(self):
     modules = glob.glob(self.config['pluginsDirectory'] + '/*.py')
     maybe_plugins = [import_module('backend.plugins.' + basename(f)[:-3]).CBPlugin() for f in modules
-                     if isfile(f) and not (f.endswith('__init__.py') or f.endswith('abstract_plugin.py') or f.endswith('torrent.py'))]
+                     if isfile(f) and not f.endswith('__init__.py')]
 
     # filter these based on if they have a 'verify_cbplugin' method
     for plugin in maybe_plugins:
