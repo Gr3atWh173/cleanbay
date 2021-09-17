4a92e6ba90d366e6ad1b50ba3b7edfdda66fa465
Gr3atWh173 <Gr3atWh173@users.noreply.github.com>
2021-09-17 14:23:04 +0530

    update test

--- app.py
+++ app.py
@@ -2,10 +2,16 @@
 
 # just a test-bed for now
 from backend import Backend
+import asyncio
+import time
 
 back = Backend()
 
 print(back.config, back.plugins)
 
-print(back.search_plugin("game of thrones", "eztv"))
+start = time.time()
+results = asyncio.get_event_loop().run_until_complete(back.search_plugins("fast and furious", []))
+elapsed = time.time() - start
+print("async: took {} seconds to search {} plugins.".format(elapsed, len(back.plugins)))
 
+print(results[0])
