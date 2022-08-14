4cdeadd5111bff05ac3731db0b5807b038bb2c6c
randeep-singh-shiryam <randeep.singh@shiryam.com>
2022-08-14 13:10:02 +0530

    Restructuring for dep injection

---
+++ cleanbay/cache_manager/abstract_cache_manager.py
@@ -0,0 +1,32 @@
+"""Contains the cache manager interface/abstract class"""
+from abc import ABC, abstractmethod
+
+
+class AbstractCacheManager(ABC):
+  """All cache managers must be derived from this class"""
+
+  @abstractmethod
+  def store(self, search_term: str, plugins: list, listings: list):
+    """Stores a search result into the cache.
+
+    Arguments:
+    search_term (str): The string that was searched.
+    plugins (list): List of Plugin objects used in the search.
+    listings (list): List of Torrents returned from the search.
+
+    """
+    pass
+
+  @abstractmethod
+  def read(self, search_term: str, plugins: list) -> list:
+    """Reads an item from the cache.
+
+    Arguments:
+      search_term (str): The string that was searched.
+      plugins (list): List of Plugin objects used in the search.
+
+    Returns:
+      A list of Torrents.
+
+    """
+    pass
---
+++ cleanbay/plugins_manager/__init__.py
@@ -0,0 +1,2 @@
+# pylint: disable=missing-module-docstring
+from .plugins_manager import PluginsManager, NoPluginsError
